4c4c988cfbafb8080167236807e2885e3b118c8d
unknown <sudheermulesoft01@gmail.com>
2026-05-28 22:56:51 +0530

    api doc added

--- 05-variables.sh
+++ 05-variables.sh
@@ -5,7 +5,7 @@ START_TIME=$(date +%s) # executes date value in seconds and assign that value to
 
 sleep 10 # Script will wait for 10 seconds
 
-END_TIME=$(date %s) # executes date value in seconds and assign that value to END_TIME
+END_TIME=$(date +%s) # executes date value in seconds and assign that value to END_TIME
 
 TOTAL_TIME=$(($END_TIME-$START_TIME)) # executes arithematic operation for start time and end time and stores the value in TOTAL_TIME
 
