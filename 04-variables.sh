75d24a256e547e7a0d9f11b429413c61114c428d
unknown <sudheermulesoft01@gmail.com>
2026-05-28 22:40:16 +0530

    api doc added

--- 04-variables.sh
+++ 04-variables.sh
@@ -8,4 +8,4 @@ echo "User name is $USER_NAME" #Printing the username
 echo "Enter your password::"
 read -s PASSWORD # -s is silent which means the password we enter will not visible on screen
 
-echo "Password is $PASSWORD
+echo "Password is $PASSWORD"
