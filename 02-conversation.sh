b09bef16c88bc32a2b5adfbabb49a881e024548d
unknown <sudheermulesoft01@gmail.com>
2026-05-28 21:57:30 +0530

    api doc added

---
+++ 02-conversation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+$PERSON1=Trump # Declare variable PERSON1
+$PERSON2=Iran # Declare variable PERSON2
+
+echo "$PERSON1: Hey $PERSON2, We are going to blast you!"
+echo "$PERSON2: Yes please, you are welcome"
+echo "$PERSON1: Hey $PERSON2, I am serious"
+echo "$PERSON2: Hey $PERSON1, We are also serious"
