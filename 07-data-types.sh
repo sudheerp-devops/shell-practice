f328d87903f430f952e14e6f7de0873cf2528038
unknown <sudheermulesoft01@gmail.com>
2026-05-31 06:46:14 +0530

    api doc added

---
+++ 07-data-types.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+NUM1=10
+NUM2=20
+
+TOTAL=(($SUM1+$SUM2))
+
+echo "Total: $TOTAL"
+
+MOVIES=("RRR" "Varanasi" "Pushpa")
+
+echo "All movies are: $(MOVIES[@])"
+
+echo "First movie in the list: $(MOVIES[0])"
+
+echo "Second movie in the list: $(MOVIES[1])"
+
+echo "Third movie in the list: $(MOVIES[2])"
+
