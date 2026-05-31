1396b3135afea35a9eeeea4d314f803b7a867f55
unknown <sudheermulesoft01@gmail.com>
2026-05-31 07:24:54 +0530

    api doc added

---
+++ 08-conditions.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+NUMBER=$1
+
+# gt - greater than
+# lt - less than
+# eq - equal to
+# ne - not equal
+# ge - greater than or equal
+# le - less than or equal
+ 
+if [ $NUMBER -gt 20 ]; then
+ echo "Given number $NUMBER is greater than 20"
+elif [ $NUMBER -eq 20 ]; then
+ echo "Given number $NUMBER is equal to 20"
+else
+ echo "Given number $NUMBER is less than 20"
+fi
